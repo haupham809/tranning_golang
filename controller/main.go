be3a705a705e51289b24f4e3c7c6f4890c75b7c6
haupham809 <haupham404@gmail.com>
2022-01-24 17:00:39 +0700

    add api product

--- controller/main.go
+++ controller/main.go
@@ -2,13 +2,19 @@ package main
 import (
 	"github.com/labstack/echo"
 	"tranning_golang/model/product"
+	"tranning_golang/model/topping"
+	"tranning_golang/model/coupon"
+	"tranning_golang/model/order"
 	
 )
 
 func main() {
 	e := echo.New()
-	e.GET("/check", product.DetailProduct)
-
+	e.GET("/toppingforproduct", topping.ToppingForProduct)
+	e.GET("/coupon",coupon.GetCoupon)
+	e.GET("/detailproduct", product.DetailProduct)
+	e.GET("/sizeproduct", product.SizeProduct)
+	e.GET("/cancelorder", order.Updateordercancel)
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
