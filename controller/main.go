098b0db7c8dae345670a2accf9a9156271054956
vandai <vandai@vn-cubesystem.com>
2022-01-25 11:11:47 +0700

    update api

--- controller/main.go
+++ controller/main.go
@@ -1,20 +1,24 @@
 package main
+
 import (
-	"github.com/labstack/echo"
-	"tranning_golang/model/product"
-	"tranning_golang/model/topping"
 	"tranning_golang/model/coupon"
 	"tranning_golang/model/order"
-	
+	"tranning_golang/model/product"
+	"tranning_golang/model/topping"
+
+	"github.com/labstack/echo"
 )
 
 func main() {
 	e := echo.New()
 	e.GET("/toppingforproduct", topping.ToppingForProduct)
-	e.GET("/coupon",coupon.GetCoupon)
+	e.GET("/coupon", coupon.GetCoupon)
 	e.GET("/detailproduct", product.DetailProduct)
 	e.GET("/sizeproduct", product.SizeProduct)
 	e.GET("/cancelorder", order.Updateordercancel)
+	e.POST("/order", order.Order)
+	e.GET("/products", product.GetProduct)
+	e.GET("/couponbyid", coupon.GetCouponByUserLogin)
+	e.GET("/getsuggest", product.GetProductSuggest)
 	e.Logger.Fatal(e.Start(":1323"))
 }
-
