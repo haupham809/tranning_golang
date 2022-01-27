dc2fec6013d7a500ec755e2bdd852fa6ba758d91
haupham809 <haupham404@gmail.com>
2022-01-27 17:09:45 +0700

    add cap nhat type user batch cap nhat 0 h hang ngay

--- controller/main.go
+++ controller/main.go
@@ -43,6 +43,12 @@ type Payment struct {
 }
 
 func main() {
+	
+    c := cron.New()
+	c.AddFunc("* 0 * * *", CronTab)
+
+	c.Start()
+	
 
 	e := echo.New()
 	e.GET("/toppingforproduct", topping.ToppingForProduct)
@@ -54,56 +60,82 @@ func main() {
 	e.POST("/savecreditcard", payment.SaveCreditCard)
 	e.POST("/updatepaymentcancel", payment.Updatepaymentcancel)
 	e.POST("/refundmomo", payment.Refundmomo)
-	// e.PUT("/updateorder", order.UpdateOrder)
+	e.PUT("/updateorder", order.UpdateOrder)
 	e.POST("/order", order.Order)
 	e.GET("/products", product.GetProduct)
 	e.GET("/couponbyid", coupon.GetCouponByUserLogin)
 	e.GET("/getsuggest", product.GetProductSuggest)
+	
 	e.Logger.Fatal(e.Start(":1323"))
 
-	c := cron.New()
-	c.AddFunc("@every 1h", CronTab)
-	c.AddFunc("TZ=Asia/Bangkok 30 04 * * * *", func() { fmt.Println("Runs at 04:30 Bangkok time every day") })
-	c.AddFunc("@hourly", func() { fmt.Println("Every hour") })
-	c.AddFunc("@every 0h0m1s", func() { fmt.Println("Every second") })
-	c.Start()
-
-	// Funcs are invoked in their own goroutine, asynchronously.
-	go CronTab()
-	// Funcs may also be added to a running Cron
-	c.AddFunc("@daily", func() { fmt.Println("Every day") })
-
-	// Added time to see output
-	time.Sleep(10 * time.Second)
-
-	c.Stop() // Stop the scheduler (does not stop any jobs already running).
+	 
 }
-
+// kh moi type=2
+// kh tn type =1
 func CronTab() {
+	
 	if connectdb.Connnectdb() {
 		var result []User
 		tx := connectdb.DB.Begin()
 		connectdb.DB.Raw("select * from t_user").Scan(&result)
+		fmt.Println(len(result)) 
 		for i := 0; i < len(result); i++ {
 			var s = strconv.Itoa(result[i].ID)
-			if getCountPaymentID(s) > 7 {
+			if getCountdate(s) <= 7 {
 				err := tx.Table("t_user").Where("id = ?", result[i].ID).Update("type", 2).Error
 				if err != nil {
 					tx.Rollback()
 					errorapi := messageapi.Objectapi{
 						Status:  500,
-						Message: "create error",
+						Message: "update error",
 					}
 					writelog.Writelog(errorapi)
 				} else {
 					tx.Commit()
 					successapi := messageapi.Objectapi{
 						Status:  200,
-						Message: "create success",
+						Message: "update success",
+					}
+					writelog.Writelog(successapi)
+				}
+			}else if getCountorder(s) > 10 {
+				err := tx.Table("t_user").Where("id = ?", result[i].ID).Update("type", 1).Error
+				if err != nil {
+					tx.Rollback()
+					errorapi := messageapi.Objectapi{
+						Status:  500,
+						Message: "update error",
+					}
+					writelog.Writelog(errorapi)
+				} else {
+					tx.Commit()
+					successapi := messageapi.Objectapi{
+						Status:  200,
+						Message: "update success",
+					}
+					writelog.Writelog(successapi)
+				}
+
+			}else{
+				err := tx.Table("t_user").Where("id = ?", result[i].ID).Update("type", 0).Error
+				if err != nil {
+					tx.Rollback()
+					errorapi := messageapi.Objectapi{
+						Status:  500,
+						Message: "update error",
+					}
+					writelog.Writelog(errorapi)
+				} else {
+					tx.Commit()
+					successapi := messageapi.Objectapi{
+						Status:  200,
+						Message: "update success",
 					}
 					writelog.Writelog(successapi)
 				}
 			}
+
+
 
 		}
 	} else {
@@ -115,18 +147,18 @@ func CronTab() {
 	}
 }
 
-func Getpayment(id string) User {
-	var result []User
-
-	connectdb.DB.Raw("SELECT  * FROM user where id = " + id).Scan(&result)
-	return result[0]
-}
-
-func getCountPaymentID(id string) int {
-	var countn int64
-	var result []Payment
-	connectdb.DB.Raw("select * from t_payment where user_id = " + id).Scan(&result)
-	s := strconv.FormatInt(countn, 10) // s == "97" (decimal))
-	fmt.Println("long kute :" + s)
+func getCountorder(id string) int {
+	var result  []order.T_order
+	connectdb.DB.Select("*").Table("t_order").Where("user_id = ?",id).Scan(&result)
+	
 	return len(result)
+}
+type GetDateuser struct {
+	Date int 
+}
+func getCountdate(id string) int {
+	var result  GetDateuser
+	connectdb.DB.Select("DATEDIFF(CURRENT_TIMESTAMP ,date_created) as date").Table("t_user").Where("id = ?",id).Scan(&result)
+	fmt.Println(result)
+	return result.Date
 }
